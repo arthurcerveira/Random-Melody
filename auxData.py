904ee5240d79773292ff40beeb6883cf42fd8460
ArthurCerveira <aacerveira@inf.ufpel.edu.br>
2019-05-27 15:34:28 -0300

    Faz alterações em métodos para permitir 2 oitavas

--- auxData.py
+++ auxData.py
@@ -4,6 +4,6 @@ notes_lenght = (("whole", 64),
                 ("eighth", 8),
                 ("sixteenth", 4))
 
-notes_value = ('C', 'D', 'E', 'F', 'G', 'A', 'B')
+notes_value = ('C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2', 'C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3')
 
 
--- classData.py
+++ classData.py
@@ -6,7 +6,8 @@ class MusicalNote(object):
     def __init__(self):
         self.on_or_off = True if randint(1, 10) < 10 else False
         if self.on_or_off:
-            self.value = notes_value[randint(0, 6)]
+            self.value = randint(0, 13)
+            self.note = notes_value[self.value]
         self.lenght = notes_lenght[randint(0, 4)]
 
 
@@ -16,17 +17,22 @@ class MelodyBar(object):
 
     def generate_melody(self, number_bars):
         lenght_bar = 0
+        last_value = 0
         while lenght_bar != 64 * number_bars:
             note = MusicalNote()
             if note.lenght[1] + lenght_bar <= 64 * number_bars:
-                self.notes.append(note)
-                lenght_bar += note.lenght[1]
-                # print(note.lenght[1])
-                # print(lenght_bar)
-
+                if note.on_or_off:
+                    if last_value - 5 < note.value < last_value + 5:
+                        self.notes.append(note)
+                        lenght_bar += note.lenght[1]
+                        last_value = note.value
+                else:
+                    self.notes.append(note)
+                    lenght_bar += note.lenght[1]
+                    
     def print_melody(self):
         for note in self.notes:
             if note.on_or_off:
-                print(str(note.value) + " " + str(note.lenght[1]))
+                print(str(note.note) + " " + str(note.lenght[1]))
             else:
                 print("Break " + str(note.lenght[1]))
