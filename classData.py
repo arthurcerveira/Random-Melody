ddbbb000dc34386da4adf390c36a15a17eefd684
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2019-08-01 23:22:04 -0300

    Adiciona restrições para melodia e mais umas gambiarras

--- classData.py
+++ classData.py
@@ -6,11 +6,12 @@ from synthesizer import Synthesizer, Player, Waveform
 class MusicalNote(object):
     def __init__(self):
         self.on_or_off = True if randint(1, 10) < 10 else False
+        self.value = 0
         if self.on_or_off:
             # Notas de C3 a B4
             self.value = randint(0, 13)
             self.note = notes_value[self.value]
-        self.lenght_float = notes_lenght[randint(0, 4)]
+        self.lenght = notes_lenght[randint(0, 4)]
 
 
 class MelodyBar(object):
@@ -19,29 +20,39 @@ class MelodyBar(object):
 
     def generate_melody(self, number_bars, max_interval):
         lenght_bar = 0
-        last_note = 0
+        last_note = MusicalNote()
 
         # A duração de um tempo é 64
         while lenght_bar != 64 * number_bars:
             note = MusicalNote()
+
+            # Impede que a primeira nota seja uma pausa
+            if lenght_bar == 0 and not note.on_or_off:
+                continue
+
+            # Impede que ocorram duas pausas seguidas
+            if last_note.on_or_off is False and note.on_or_off is False:
+                continue
+
             # Testa se a nota ultrapassa o limite de tempo
-            if note.lenght_float + lenght_bar <= 64 * number_bars:
+            if note.lenght + lenght_bar <= 64 * number_bars:
                 if note.on_or_off:
                     # Não permite que a nota seja tenha um intervalo maior que o intervalo maximo
-                    if last_note - max_interval < note.value < last_note + max_interval:
+                    if last_note.value - max_interval < note.value < last_note.value + max_interval:
                         self.notes.append(note)
-                        lenght_bar += note.lenght_float
-                        last_note = note.value
+                        lenght_bar += note.lenght
+                        last_note = note
                 else:
                     self.notes.append(note)
-                    lenght_bar += note.lenght_float
-                    
+                    lenght_bar += note.lenght
+                    last_note = note
+
     def print_melody(self):
         for note in self.notes:
             if note.on_or_off:
-                print(str(note.note[0]) + " " + str(note.lenght_float))
+                print(str(note.note[0]) + " " + str(note.lenght))
             else:
-                print("Break " + str(note.lenght_float))
+                print("Break " + str(note.lenght))
 
 
 class MelodyPlayer(object):
@@ -53,11 +64,11 @@ class MelodyPlayer(object):
         self.player.open_stream()
 
         for note in melody.notes:
-            time = note.lenght_float/16 * 60/bpm
+            time = note.lenght/16 * 60/bpm
 
             if note.on_or_off:
                 frequency = note.note[1]
                 self.player.play_wave(self.synthesizer.generate_constant_wave(frequency, time))
             else:
-                # caso seja uma pausa, não toca nota
+                # Caso seja uma pausa, não toca nota
                 self.player.play_wave(self.synthesizer.generate_constant_wave(0, time))
