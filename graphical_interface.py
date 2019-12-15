5a41d542ac476fd5bb270c117a911149bb529396
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2019-12-15 15:28:24 -0300

    Refatora nome de variaveis e funções

--- graphical_interface.py
+++ graphical_interface.py
@@ -5,17 +5,16 @@
 # Created by: PyQt5 UI code generator 5.13.0
 #
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 from melody_generator import MelodyBar, MelodyPlayer
 
 
 class Ui_MainWindow(object):
     def __init__(self):
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget = QtWidgets.QWidget(main_window)
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.user_feedback = QtWidgets.QLabel(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar = QtWidgets.QStatusBar(main_window)
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.bars_input = QtWidgets.QLineEdit(self.centralwidget)
@@ -29,9 +28,9 @@ class Ui_MainWindow(object):
         self.melody_bar = MelodyBar()
         self.melody_player = MelodyPlayer()
 
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(359, 495)
+    def setup_ui(self, window):
+        window.setObjectName("MainWindow")
+        window.resize(359, 495)
 
         self.centralwidget.setObjectName("centralwidget")
 
@@ -76,17 +75,17 @@ class Ui_MainWindow(object):
         self.user_feedback.setGeometry(QtCore.QRect(100, 340, 250, 20))
         self.user_feedback.setText("")
         self.user_feedback.setObjectName("user_feedback")
-        MainWindow.setCentralWidget(self.centralwidget)
+        window.setCentralWidget(self.centralwidget)
 
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        window.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslate_ui(window)
+        QtCore.QMetaObject.connectSlotsByName(window)
 
-    def retranslateUi(self, MainWindow):
+    def retranslate_ui(self, window):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        window.setWindowTitle(_translate("MainWindow", "Melody Genereator"))
         self.generate_melody_btn.setText(_translate("MainWindow", "Generate Melody"))
         self.play_btn.setText(_translate("MainWindow", "Play Melody"))
         self.save_btn.setText(_translate("MainWindow", "Save as WAV"))
@@ -149,10 +148,10 @@ class Ui_MainWindow(object):
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
+    main_window = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    ui.setup_ui(main_window)
+    main_window.show()
     sys.exit(app.exec_())
 
 
